d25c5caa8e9f236616c20676d1ea40688fb97598
Diyorbek-Sultanov <diyorbeksultonov1@mail.ru>
2023-12-14 09:41:46 +0500

    feat: billboard suspense

--- app/(routes)/(root)/page.tsx
+++ app/(routes)/(root)/page.tsx
@@ -1,16 +1,16 @@
-import { fetchBillboard } from '@/actions/fetch-billboard'
 import Billboards from '@/components/billboards'
 import Container from '@/components/container'
 import ProductReel from '@/components/products/product-reel'
+import { Suspense } from 'react'
 
 const HomePage = async () => {
-	const billboard = await fetchBillboard('8009f659-2b70-4723-a535-f759448831d1')
-
 	return (
 		<Container>
-			<div className='space-y-10 pb-10'>
-				<Billboards data={billboard} />
-			</div>
+			<Suspense fallback={<Billboards.Skeleton />}>
+				<div className='space-y-10 pb-10'>
+					<Billboards billboardId={'8009f659-2b70-4723-a535-f759448831d1'} />
+				</div>
+			</Suspense>
 			<ProductReel
 				title={'Products'}
 				subTitle={'More products'}
--- components/billboards.tsx
+++ components/billboards.tsx
@@ -1,16 +1,29 @@
-import { IBillboard } from '@/types'
+import { fetchBillboard } from '@/actions/fetch-billboard'
+import Skeleton from '@/components/ui/skeleton'
 
-const Billboards: React.FC<{ data: IBillboard }> = ({ data }) => {
+const Billboards = async ({ billboardId }: { billboardId: string }) => {
+	const billboard = await fetchBillboard(billboardId)
+
 	return (
 		<div className='p-4 sm:p-6 lg:p-7 rounded-xl overflow-hidden'>
 			<div
 				className='rounded-xl relative aspect-square bg-cover bg-no-repeat md:aspect-[2.6/1] overflow-hidden bg-center'
-				style={{ backgroundImage: `url(${data.imageUrl})` }}>
+				style={{ backgroundImage: `url(${billboard.imageUrl})` }}>
 				<div className='h-full w-full flex items-center justify-center text-center gap-y-8'>
 					<div className='font-bold text-3xl sm:text-5xl lg:text-6xl sm:max-w-xl max-w-xs'>
-						{data.label}
+						{billboard.label}
 					</div>
 				</div>
+			</div>
+		</div>
+	)
+}
+
+Billboards.Skeleton = function BillboardsSkeleton() {
+	return (
+		<div className='space-y-10 pb-10'>
+			<div className='p-4 sm:p-6 lg:p-7 rounded-xl overflow-hidden'>
+				<Skeleton className='aspect-square rounded-xl md:aspect-[2.6/1]' />
 			</div>
 		</div>
 	)
